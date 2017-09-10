126b708e89aa73b76b4519c4d633b528d4a376f1
Hajime Hoshi <hajimehoshi@gmail.com>
2017-09-11 07:28:28 +0900

    Add license comments

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// Copyright 2017 The Ebiten Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 var script = "script";
 var snippets = "https://24l1khds95.execute-api.us-east-1.amazonaws.com/prod/";
 var defaultProg = `package main
