9fc21a7115c1bece470e6041442fe6cd677a7534
Hajime Hoshi <hajimehoshi@gmail.com>
2017-09-17 15:57:07 +0900

    Fix indentation
    Fixes #14

--- index.js
+++ index.js
@@ -29,162 +29,162 @@ func main() {
 `;
 
 function setButtonsDisabled(value) {
-    let buttons = document.querySelectorAll('.btn');
-    for (let button of buttons) {
-        button.disabled = value;
-    }
+  let buttons = document.querySelectorAll('.btn');
+  for (let button of buttons) {
+    button.disabled = value;
+  }
 }
 
 function init() {
-    Go.RedirectConsole(l => {
-        let e = document.getElementById('console');
-        e.textContent += l;
-    })
-    let el = document.getElementById('pg-editor')
-    let editor = ace.edit(el);
+  Go.RedirectConsole(l => {
+    let e = document.getElementById('console');
+    e.textContent += l;
+  })
+  let el = document.getElementById('pg-editor')
+  let editor = ace.edit(el);
 
-    // Surpress warnings of Chrome.
-    editor.$blockScrolling = Infinity;
+  // Surpress warnings of Chrome.
+  editor.$blockScrolling = Infinity;
 
-    editor.setTheme('ace/theme/sqlserver');
-    editor.getSession().setMode('ace/mode/golang');
+  editor.setTheme('ace/theme/sqlserver');
+  editor.getSession().setMode('ace/mode/golang');
 
+  setButtonsDisabled(true);
+  if (window.location.hash) {
+    let hash = window.location.hash.substring(1);
+    if (hash[0] === '/') {
+      hash = hash.substring(1);
+    }
+    fetch(`${snippets}${hash}`).then(response => {
+      if (200 <= response.status && response.status < 300)
+        return response.text();
+      return Promise.reject(response);
+    }).then(text => {
+      editor.setValue(text, -1);
+      setButtonsDisabled(false);
+    }).catch(_ => {
+      location.href = location.href.split('#')[0];
+    });
+  } else {
+    editor.setValue(defaultProg, -1);
+    Go.Compile(defaultProg).then(() => {
+      setButtonsDisabled(false);
+    });
+  }
+
+  let fontSize = parseInt(getCookie('fontsize'), 10);
+  if (fontSize === 0) {
+    fontSize = 16;
+  }
+  document.getElementById('pg-fontsize').value = fontSize;
+  editor.setFontSize(fontSize);
+
+  document.getElementById('pg-fontsize').addEventListener('change', (e) => {
+    e.preventDefault()
+    let size = +(document.getElementById('pg-fontsize').value);
+    editor.setFontSize(size);
+    setCookie('fontsize',size,365);
+  });
+
+  document.getElementById('pg-format').addEventListener('click', (e) => {
+    e.preventDefault();
     setButtonsDisabled(true);
-    if (window.location.hash) {
-        let hash = window.location.hash.substring(1);
-        if (hash[0] === '/') {
-            hash = hash.substring(1);
+    let src = editor.getValue();
+    Go.Format(src, document.getElementById('pg-imports').checked)
+      .then(s => editor.setValue(s,-1))
+      .then(() => {setButtonsDisabled(false);})
+      .catch(err => {
+        document.getElementById('console').textContent = err;
+        setButtonsDisabled(false);
+      })
+  });
+
+  document.getElementById('pg-share').addEventListener('click', (e) => {
+    e.preventDefault()
+    let share = document.getElementById('pg-share');
+    share.disabled = true;
+    fetch(`${snippets}`, {
+      method: 'POST',
+      body:   editor.getValue(),
+    }).then(response => {
+      return response.text();
+    }).then(text => {
+      location.hash = `#/${text}`;
+      share.disabled = false;
+    }).catch(err => {
+      document.getElementById('console').textContent = err;
+    });
+  });
+
+  document.getElementById('pg-run').addEventListener('click', (e) => {
+    e.preventDefault();
+    setButtonsDisabled(true);
+    Go.Compile(editor.getValue())
+      .then((src) => {
+        document.getElementById('console').textContent = '';
+        let output = document.getElementById('output')
+        while (output.firstChild) {
+          output.removeChild(output.firstChild);
         }
-        fetch(`${snippets}${hash}`).then(response => {
-            if (200 <= response.status && response.status < 300)
-                return response.text();
-            return Promise.reject(response);
-        }).then(text => {
-            editor.setValue(text, -1);
-            setButtonsDisabled(false);
-        }).catch(_ => {
-            location.href = location.href.split('#')[0];
-        });
+        let div = document.createElement('div');
+        div.classList.add('embed-responsive');
+        div.classList.add('embed-responsive-4by3');
+        // allowfullscreen
+        let iframe = document.createElement('iframe');
+        iframe.className = 'embed-responsive-item';
+        iframe.sandbox = 'allow-forms allow-scripts allow-same-origin allow-modals allow-popups';
+        div.appendChild(iframe);
+        output.appendChild(div)
+        let doc = iframe.contentWindow.document;
+        let script = doc.createElement('script');
+        script.textContent = 'if (top.goPrintToConsole) { window.goPrintToConsole = top.goPrintToConsole; }';
+        doc.head.appendChild(script);
+        script = doc.createElement('script');
+        script.textContent = src;
+        doc.body.appendChild(script);
+        setButtonsDisabled(false);
+      })
+      .catch((err) => {
+        document.getElementById('console').textContent = err;
+        setButtonsDisabled(false);
+      });
+  });
+
+  document.getElementById('nav-about').addEventListener('click', e => {
+    e.preventDefault();
+    let playground = document.getElementById('container-playground');
+    let about = document.getElementById('container-about');
+    if (playground.style.display === 'block' || playground.style.display === '') {
+      playground.style.display = 'none';
+      about.style.display = 'block';
     } else {
-        editor.setValue(defaultProg, -1);
-        Go.Compile(defaultProg).then(() => {
-            setButtonsDisabled(false);
-        });
+      playground.style.display = 'block';
+      about.style.display = 'none';
     }
-
-    let fontSize = parseInt(getCookie('fontsize'), 10);
-    if (fontSize === 0) {
-        fontSize = 16;
-    }
-    document.getElementById('pg-fontsize').value = fontSize;
-    editor.setFontSize(fontSize);
-
-    document.getElementById('pg-fontsize').addEventListener('change', (e) => {
-        e.preventDefault()
-        let size = +(document.getElementById('pg-fontsize').value);
-        editor.setFontSize(size);
-        setCookie('fontsize',size,365);
-    });
-
-    document.getElementById('pg-format').addEventListener('click', (e) => {
-        e.preventDefault();
-        setButtonsDisabled(true);
-        let src = editor.getValue();
-        Go.Format(src, document.getElementById('pg-imports').checked)
-            .then(s => editor.setValue(s,-1))
-            .then(() => {setButtonsDisabled(false);})
-            .catch(err => {
-                document.getElementById('console').textContent = err;
-                setButtonsDisabled(false);
-            })
-    });
-
-    document.getElementById('pg-share').addEventListener('click', (e) => {
-        e.preventDefault()
-        let share = document.getElementById('pg-share');
-        share.disabled = true;
-        fetch(`${snippets}`, {
-            method: 'POST',
-            body:   editor.getValue(),
-        }).then(response => {
-            return response.text();
-        }).then(text => {
-            location.hash = `#/${text}`;
-            share.disabled = false;
-        }).catch(err => {
-            document.getElementById('console').textContent = err;
-        });
-    });
-
-    document.getElementById('pg-run').addEventListener('click', (e) => {
-        e.preventDefault();
-        setButtonsDisabled(true);
-        Go.Compile(editor.getValue())
-            .then((src) => {
-                document.getElementById('console').textContent = '';
-                let output = document.getElementById('output')
-                while (output.firstChild) {
-                    output.removeChild(output.firstChild);
-                }
-                let div = document.createElement('div');
-                div.classList.add('embed-responsive');
-                div.classList.add('embed-responsive-4by3');
-                // allowfullscreen
-                let iframe = document.createElement('iframe');
-                iframe.className = 'embed-responsive-item';
-                iframe.sandbox = 'allow-forms allow-scripts allow-same-origin allow-modals allow-popups';
-                div.appendChild(iframe);
-                output.appendChild(div)
-                let doc = iframe.contentWindow.document;
-                let script = doc.createElement('script');
-                script.textContent = 'if (top.goPrintToConsole) { window.goPrintToConsole = top.goPrintToConsole; }';
-                doc.head.appendChild(script);
-                script = doc.createElement('script');
-                script.textContent = src;
-                doc.body.appendChild(script);
-                setButtonsDisabled(false);
-            })
-            .catch((err) => {
-                document.getElementById('console').textContent = err;
-                setButtonsDisabled(false);
-            });
-    });
-
-    document.getElementById('nav-about').addEventListener('click', e => {
-        e.preventDefault();
-        let playground = document.getElementById('container-playground');
-        let about = document.getElementById('container-about');
-        if (playground.style.display === 'block' || playground.style.display === '') {
-            playground.style.display = 'none';
-            about.style.display = 'block';
-        } else {
-            playground.style.display = 'block';
-            about.style.display = 'none';
-        }
-    });
+  });
 }
 
 window.addEventListener('load', init);
 
 function setCookie(cname, cvalue, exdays) {
-    let d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = 'expires='+ d.toUTCString();
-    document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+  let d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = 'expires='+ d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
 
 function getCookie(cname) {
-    let name = cname + '=';
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
+  let name = cname + '=';
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
     }
-    return '';
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
 }
