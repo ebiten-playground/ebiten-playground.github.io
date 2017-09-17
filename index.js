8aff7f02f25d5b15ee01e546fc1ce3e4d1fb41d7
Hajime Hoshi <hajimehoshi@gmail.com>
2017-09-18 01:00:12 +0900

    Refactoring

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ function setCookie(cname, cvalue, exdays) {
   let d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   let expires = 'expires='+ d.toUTCString();
-  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+  document.cookie = `${cname}=${cvalue};${expires};path=/`;
 }
 
 function getCookie(cname) {
